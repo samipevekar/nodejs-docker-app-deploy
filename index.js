c654ef71a1bbe17fff2c8c7c5365c804e9b4f2ad
samipevekar <samipevekar499@gmail.com>
2026-09-06 17:30:38 +0530

    change index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-    return res.json({msg: "Hello World"})
+    return res.json({msg: "Hello World v2!"})
 })
 
 app.listen(PORT, () => {
